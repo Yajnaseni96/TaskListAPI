b06528f107a20012b683962c0d31b229a8fe5048
Yajnaseni96 <meyajnasenipanda@gmail.com>
2020-12-28 13:07:02 +0530

    updated reducer and action

---
+++ src/store/reducers/listReducer.ts
@@ -0,0 +1,18 @@
+import {ListState, ListsAction} from '../types';
+
+const initialState: ListState = {
+    lists: {},
+    listIdToDelete: '',
+    listToEdit: null,
+    listById: null,
+    selectedList: null,
+    taskToDelete: null,
+    taskToEdit: null
+}
+
+export default (state= initialState, action: ListsAction): ListState => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
