112f87454621191422ffa67fe1da2d45bdcc1591
Yajnaseni96 <meyajnasenipanda@gmail.com>
2020-12-28 16:54:16 +0530

    updated listReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ReactReduxContext } from 'react-redux';
 import App from './App';
+import {Provider} from 'react-redux';
+import store from './store/store';
+import  '../node_modules/bulma/css/bulma.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
