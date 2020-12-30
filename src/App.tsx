47b34c6bf58b9032725c70c46d82fb724f56be9d
Yajnaseni96 <meyajnasenipanda@gmail.com>
2020-12-30 16:31:31 +0530

    Components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
-
-function App() {
+import Header from './components/Header'
+const App:FC =() =>{
   return (
     <div className="App">
-      
+      <Header title="Task List App" subtitle="Create some lists and add some tasks to each list"/>
     </div>
   );
 }
